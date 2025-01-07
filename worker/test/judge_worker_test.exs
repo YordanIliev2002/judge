7bb4ebd87c728baa836db3103f3cc479d53c961c
Yordan <jordan.iliev501@gmail.com>
2025-01-07 19:53:35 +0200

    Add rabbit mq

--- worker/test/judge_worker_test.exs
+++ worker/test/judge_worker_test.exs
@@ -2,7 +2,6 @@ defmodule JudgeWorkerTest do
   use ExUnit.Case
   doctest JudgeWorker
 
-  test "greets the world" do
-    assert JudgeWorker.hello() == :world
+  test "app starts properly" do
   end
 end
